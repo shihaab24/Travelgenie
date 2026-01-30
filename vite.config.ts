70b2838313975c66f4d9b53eb73b5df298a90d8d
shihaab24 <shihaabkhan2405@gmail.com>
2026-01-30 15:29:53 +0530

    Auto-sync from App Studio

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,29 @@
 import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react-swc'
-    import { fileURLToPath, URL } from "node:url";
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-    export default defineConfig({
-      plugins: [react()],
-      resolve: {
-        alias: {
-          "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
-      },
-      server: {
-        allowedHosts: true,
-        host: '0.0.0.0',
-        port: 5173,
-        cors: false,
-        hmr: {
-          port: 5173,
-          host: '0.0.0.0'
-        },
-        watch: {
-          usePolling: true,
-          interval: 1000
-        }
-      },
-      optimizeDeps: {
-        force: true
-      },
-      css: {
-        postcss: './postcss.config.js'
-      }
-    })
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    allowedHosts: true,
+    host: '0.0.0.0',
+    port: 5173,
+    cors: false,
+    hmr: {
+      port: 5173,
+      host: '0.0.0.0'
+    },
+    watch: {
+      usePolling: true,
+      interval: 1000
+    }
+  },
+  css: {
+    postcss: './postcss.config.js'
+  }
+})
